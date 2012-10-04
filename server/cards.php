bfa6a1a8632c37b79337e0135407eccc5a48e73e
Dustin Server <info@dustytech.com>
2012-10-04 19:47:34 -0400

    Finished cards.php

--- server/cards.php
+++ server/cards.php
@@ -1,3 +1,14 @@
+<head>
+<style>
+input[type=text] {
+width: 75%;
+}
+form {
+border: 1px solid black;
+padding: 5px;
+}
+</style>
+</head>
 <?php
     include(dirname(__FILE__)."/modules/cards.php");
     
@@ -14,11 +25,27 @@ Show id: <input type="text" name="show_id"/> <br/>
     }
     
     $cards = allCards($show_id);
+
+    $keys = array_keys($cards);
+
+    $cards[0] = array("json" => json_decode('{
+  "picture" : "<enter url>",
+  "prizeName" : "<enter prize>",
+  "prizeDescription" : "<enter description>",
+  "name" : "<enter name>",
+  "squares" : [
     
-    foreach($cards as $card_id => $card) {
+  ]
+}'));
+
+    $keys[] = 0;
+    
+    foreach($keys as $card_id) {
+        $card = $cards[$card_id];
+        echo "<h1>{$card["json"]->name}</h1>";
         ?>
 <form action="edit_card.php">
-<input type="hidden" name="card_id" value="$card_id"/>
+<input type="hidden" name="card_id" value="<?php echo $card_id ?>"/>
 <?php
     foreach($card["json"] as $key => $value) {
         
@@ -28,11 +55,12 @@ Show id: <input type="text" name="show_id"/> <br/>
             
             echo "<b>$key </b><br/>\n";
             
-            for(int i = 0; i < 24; i++)
-                echo '* <input type='text' name='json_{$key}_$i' value='$value[$i]'/> <br/>\n";
+            for($i = 0; $i < 24 || $i < count($value); $i++)
+                echo "* <input type='text' name='json_{$key}_$i' value='$value[$i]'/> <br/>\n";
         }
     }
     ?>
+<input type="submit" /> <br/>
 </form>
 <?php
     }
