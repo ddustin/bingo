421b1d9b77bda691ea6b7f4199485bb3b43882b1
Dustin Dettmer <dustinpaystaxes@gmail.com>
2012-10-04 15:34:28 -0700

    dump of all cards.

--- server/cards.php
+++ server/cards.php
@@ -1,18 +1,21 @@
-<html>
-<body>
 <?php
+    include(dirname(__FILE__)."/modules/user.php");
     
     $show_id = intval($_REQUEST['show_id']);
     
     if(!$show_id) {
-        
         ?>
 <form>
 Show id: <input type="text" name="show_id"/> <br/>
 <input type="submit"/>
 </form>
 <?php
-        
         exit;
     }
+    
+    $cards = allCards($show_id);
+    
+    echo "<pre>";
+    print_r($cards);
 ?>
+
---
+++ server/modules/cards.php
@@ -0,0 +1,32 @@
+<?php
+    include_once(dirname(__FILE__)."/database.php");
+    
+    /******* Functions *********
+     
+     allCards(show_id)
+     // Returns all the data for all cards
+     // The format is an array of hash tables. Each hash table has two keys:
+     // "id" => the bingo_card_id
+     // "json" => the bingo card json decoded into a php object
+     // 
+     // On error or no matches, returns an empty array.
+    */
+    
+    function allCards(show_id) {
+        
+        global $database;
+        
+        $show_id = intval($show_id);
+        
+        $query = "select `id`, `json` from `bingo_card` where `show_id` = $show_id limit 1000";
+        
+        $res = $database->query($query);
+        
+        $results = array();
+        
+        while($row = mysql_fetch_row($res))
+            $results[] = array("id" => $row[0], "json" => json_decode($row[1]));
+        
+        return $results;
+    }
+?>
