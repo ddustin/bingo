7f5ac73254f0081ead80f55e56455a9bc954aef0
Dustin Dettmer <dustinpaystaxes@gmail.com>
2012-10-04 15:36:26 -0700

    missing dollar sign.

--- server/modules/cards.php
+++ server/modules/cards.php
@@ -12,7 +12,7 @@
      // On error or no matches, returns an empty array.
     */
     
-    function allCards(show_id) {
+    function allCards($show_id) {
         
         global $database;
         
