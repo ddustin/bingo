8de783c0b9c9ac8fda3611db0fcd29a0444bdf2b
Dustin Dettmer <dustinpaystaxes@gmail.com>
2012-10-04 16:33:03 -0700

    First pass on card edit form.

--- server/cards.php
+++ server/cards.php
@@ -15,7 +15,25 @@ Show id: <input type="text" name="show_id"/> <br/>
     
     $cards = allCards($show_id);
     
-    echo "<pre>";
-    print_r($cards);
+    foreach($cards as $card_id => $card) {
+        ?>
+<form action="edit_card.php">
+<input type="hidden" name="card_id" value="$card_id"/>
+<?php
+    foreach($card["json"] as $key => $value) {
+        
+        if(!is_array($value))
+            echo "$key <input type='text' name='json_$key' value='$value'/> <br/>\n";
+        else {
+            
+            echo "<b>$key </b><br/>\n";
+            
+            for(int i = 0; i < 24; i++)
+                echo '* <input type='text' name='json_{$key}_$i' value='$value[$i]'/> <br/>\n";
+        }
+    }
+    ?>
+</form>
+<?php
+    }
 ?>
-
